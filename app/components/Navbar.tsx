263144ee8c665a89c6ad29e3b5e62915b04761a6
jupitters <sampaiobale@gmail.com>
2026-01-02 20:21:26 -0300

    navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header>
+        <nav>
+            <Link href="/" className="logo">
+                <Image src="/icons/logo.png" alt='logo' width={24} height={24} />
+            </Link>
+        </nav>
+    </header>
   )
 }
 
