e5bca74bff33a982155df8acbecf79a17a09c4c8
jupitters <sampaiobale@gmail.com>
2026-01-02 20:45:28 -0300

    css fixed

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const ExploreBtn = () => {
   return (
-    <button type="button" id="exploreBtn" className="mt-7 mx-auto" onClick={() => console.log("oi")}>
+    <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log("oi")}>
         <a href="#events">Explore Events</a>
         <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}/>
     </button>
