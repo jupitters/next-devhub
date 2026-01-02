1cf77f51924028cc284860e85f218c6036194669
jupitters <sampaiobale@gmail.com>
2026-01-02 20:18:41 -0300

    main page

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,15 @@ const page = () => {
       <p className='text-center mt-5'>Hackatons, Meetups and Conferences, All in one place.</p>
 
       <ExploreBtn />
+
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+        <ul className='events'>
+          {[1,2,3,4,5].map((event) => (
+            <li key={event}>{event}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }
