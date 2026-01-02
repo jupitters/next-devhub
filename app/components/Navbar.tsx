7196966bb54eedfe4a9f239655300fa44ec91cbf
jupitters <sampaiobale@gmail.com>
2026-01-02 20:22:46 -0300

    navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,13 @@ const Navbar = () => {
         <nav>
             <Link href="/" className="logo">
                 <Image src="/icons/logo.png" alt='logo' width={24} height={24} />
+                <p>Dev Event</p>
             </Link>
+            <ul>
+                <Link href="/">Home</Link>
+                <Link href="/">Events</Link>
+                <Link href="/">Create Event</Link>
+            </ul>
         </nav>
     </header>
   )
