eba645e2602a7613b824ca8f55f6611455c29816
jupitters <sampaiobale@gmail.com>
2026-01-03 15:09:46 -0300

    routes

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -13,6 +13,6 @@ export async function POST(req: NextRequest) {
         }
     }catch(e){
         console.error(e);
-        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'})
+        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 400 })
     }
 }
