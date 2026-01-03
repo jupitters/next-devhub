813e0f534641701f88ef0b65be428f249efa0b80
jupitters <sampaiobale@gmail.com>
2026-01-03 14:22:04 -0300

    event card

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -19,6 +19,14 @@ const EventCard = ({ title, image, slug, location, date, time }: Props) => {
           <p>{location}</p>
         </div>
         <p className="title">{title}</p>
+        <div className="datetime">
+          <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+          <p>{date}</p>
+        </div>
+        <div>
+          <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+          <p>{time}</p>
+        </div>
     </Link>
   )
 }
