a51855e9a868c72a2b750dfec2d29bdd762df6f9
jupitters <sampaiobale@gmail.com>
2026-01-03 15:09:39 -0300

    routes

---
+++ app/api/events/route.ts
@@ -0,0 +1,18 @@
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try{
+        await connectDB();
+        const formData = await req.formData();
+        let event;
+        try{
+            event = Object.fromEntries(formData.entries())
+        } catch (e){
+            return NextResponse.json({ message: 'Invalid Json data format.'}, { status: 400 })
+        }
+    }catch(e){
+        console.error(e);
+        return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'})
+    }
+}
