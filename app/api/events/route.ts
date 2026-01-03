a52f7611a7ec173d8a6e8f8d941d3d35bd5b1377
jupitters <sampaiobale@gmail.com>
2026-01-03 15:14:12 -0300

    routes

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: NextRequest) {
 
         const createdEvent = await Event.create(event);
 
-        return NextResponse.json({ message: 'Event created.', event: createdEvent })
+        return NextResponse.json({ message: 'Event created.', event: createdEvent }, { status: 201 },)
     }catch(e){
         console.error(e);
         return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 400 })
