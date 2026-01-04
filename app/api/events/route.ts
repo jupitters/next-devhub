60769896b8b334b8e21a5d7f450032cd7deec24a
jupitters <sampaiobale@gmail.com>
2026-01-04 20:09:45 -0300

    fetch events

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -39,3 +39,11 @@ export async function POST(req: NextRequest) {
         return NextResponse.json({ message: 'Event Creation Failed', error: e instanceof Error ? e.message : 'Unknown'}, { status: 400 })
     }
 }
+
+export async function GET(){
+    try{
+
+    } catch(e){
+        return NextResponse.json({ message: 'Event fetching failed', error: e}, {status: 500})
+    }
+}
