e42d2bf7d05ca67e7c99c764d095a29bb05416e0
jupitters <sampaiobale@gmail.com>
2026-01-05 17:50:09 -0300

    event page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -3,17 +3,17 @@ import { notFound } from "next/navigation";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string }) => {
-  <div>
+const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string }) => (
+  <div className="flex-row-gap-2 items-center">
     <Image src={icon} alt={alt} width={17} height={17} />
     <p>{label}</p>
   </div>
-}
+)
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string}>}) => {
   const { slug } = await params;
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
-  const { event: { description, image, overview, data, time, location, mode, agenda, audience, tags} } = await request.json();
+  const { event: { description, image, overview, date, time, location, mode, agenda, audience, tags} } = await request.json();
 
   if (!description) return notFound();
 
@@ -21,7 +21,7 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string}>})
     <section id="event">
       <div className="header">
         <h1>Event Description</h1>
-        <p className="mt-2">{description}</p>
+        <p>{description}</p>
       </div>
       <div className="details">
         <div className="content">
@@ -32,7 +32,12 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string}>})
           </section>
           <section className="flex-col-gap-2">
             <h2>Event Details</h2>
-            <p>{overview}</p>
+            <EventDetailItem icon="/icons/calendar.svg" alt="calendar" label={date} />
+            <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time} />
+            <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location} />
+            <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode} />
+            <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience} />
+            
           </section>
         </div>
         <aside className="booking">
