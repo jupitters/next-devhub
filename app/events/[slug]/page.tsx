bf48731b694f61170cce51c97dd1778e69595ca3
jupitters <sampaiobale@gmail.com>
2026-01-05 17:22:15 -0300

    event page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,5 +1,13 @@
+import { notFound } from "next/navigation";
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string}>}) => {
   const { slug } = await params;
+  const request = await fetch(`${BASE_URL}/api/events/${slug}`);
+  const { data } = await request.json();
+
+  if (!data) return notFound();
 
   return (
     <section id="event">
