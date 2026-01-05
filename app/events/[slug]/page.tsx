d2ae7a39899e0abe0d2d76168f765fd7ce998c4c
jupitters <sampaiobale@gmail.com>
2026-01-05 17:43:41 -0300

    event page

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -3,6 +3,13 @@ import { notFound } from "next/navigation";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+const EventDetailItem = ({ icon, alt, label }: { icon: string; alt: string; label: string }) => {
+  <div>
+    <Image src={icon} alt={alt} width={17} height={17} />
+    <p>{label}</p>
+  </div>
+}
+
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string}>}) => {
   const { slug } = await params;
   const request = await fetch(`${BASE_URL}/api/events/${slug}`);
@@ -19,7 +26,14 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string}>})
       <div className="details">
         <div className="content">
           <Image src={image} alt="Event Banner" width={800} height={800} className="banner"/>
-          
+          <section className="flex-col-gap-2">
+            <h2>Overview</h2>
+            <p>{overview}</p>
+          </section>
+          <section className="flex-col-gap-2">
+            <h2>Event Details</h2>
+            <p>{overview}</p>
+          </section>
         </div>
         <aside className="booking">
           <p className="text-lg font-semibold">Book Event</p>
